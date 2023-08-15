2a99359ba21c83e4370cb7115ff5a49dff613e6d
Jorge Hernandez <hernandezjorge.itlp@gmail.com>
2023-08-15 10:28:07 -0600

    LoginForm v1
    diseño del login v1

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,15 @@
+import RegisterForm from 'src/components/auth/RegisterForm.vue';
 
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('components/auth/LoginForm.vue') },
+      { path: 'registro', component: RegisterForm }
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
